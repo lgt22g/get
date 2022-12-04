082dfccd56949faaf27de5b6def9c3040332be48
lgt22g <111092753+lgt22g@users.noreply.github.com>
2022-12-03 21:56:47 -0700

    Create t.js

---
+++ t.js
@@ -0,0 +1,3 @@
+function getct() {
+  return document.querySelector("#LiveGameStudentTarget > div.StudentLayout > section > div > div.UIDiv.StudyWhileWaitingView-card > div > div > div > div > div > div > section.CardsItem.current.showing.showing-first > div.CardsItemSide.CardsItemSide--firstSide.has-text.showing-hint > div > div > div > div").innerText()
+}
