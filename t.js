83f5b0009ee37d38029e09685e34c2d6496aa9fc
lgt22g <111092753+lgt22g@users.noreply.github.com>
2022-12-03 23:53:08 -0700

    Update t.js

--- t.js
+++ t.js
@@ -1,7 +1,9 @@
 function geta() {
   return document.querySelector("section.CardsItem.current.showing.showing-first").innerText.split("\n")
 }
-
+function getc() {
+  return Number(document.querySelector("#LiveGameStudentTarget > div.StudentLayout > section > div > div.UIDiv.StudyWhileWaitingView-card > div > div > div > div > div > div > div > div.CardsList-navControl.progressIndex > span").innerText.split("/")[1])
+}
 let Cycle = {}
 
 Cycle.next = function(){
@@ -16,7 +18,10 @@ function current() {
   Cycle.next()
 }
 
+tt = {}
+dd = {}
 function gtd() {
   for (let i = 1; i <= getc(); i++) {current()}
-  return [dict, Object.fromEntries(Object.entries(dict).map(n => n.reverse()))]
+  tt = dict;
+  dd = Object.fromEntries(Object.entries(dict).map(n => n.reverse()))]
 }
