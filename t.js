da14e3014c36b1c3cbf0e27022dac00d2c24c89b
lgt22g <111092753+lgt22g@users.noreply.github.com>
2022-12-03 22:14:12 -0700

    Update t.js

--- t.js
+++ t.js
@@ -1,3 +1,22 @@
-function getct() {
-  return document.querySelector("#LiveGameStudentTarget > div.StudentLayout > section > div > div.UIDiv.StudyWhileWaitingView-card > div > div > div > div > div > div > section.CardsItem.current.showing.showing-first > div.CardsItemSide.CardsItemSide--firstSide.has-text.showing-hint > div > div > div > div").innerText()
+function geta() {
+  return document.querySelector("section.CardsItem.current.showing.showing-first").innerText.split("\n")
+}
+
+let Cycle = {}
+
+Cycle.next = function(){
+  document.querySelector("#LiveGameStudentTarget > div.StudentLayout > section > div > div.UIDiv.StudyWhileWaitingView-card > div > div > div > div > div > div > div > div.CardsList-navControl.nextButton > span > button")?.click()
+}
+
+let dict = {}
+
+function current() {
+  let o = geta();
+  dict[o[0]] = o[1]
+  Cycle.next()
+}
+
+function gtd() {
+  for (let i = 1; i <= getc(); i++) {current()}
+  return [dict, Object.fromEntries(Object.entries(dict).map(n => n.reverse()))]
 }
